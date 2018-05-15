9e90708dc15d62c65cd485e1d711c3322dd71ac9
PiotrKedra <piotr.k9750@gmail.com>
2018-05-15 18:36:30 +0200

    check if binary work but honestly imo i did it pretty bad

--- lab2/binaryTree.hs
+++ lab2/binaryTree.hs
@@ -12,3 +12,27 @@ insert (Node a left right) b
     |a>b = (Node a (insert left b) right)
     |a<b = (Node a left (insert right b))
 --insert work but it work on copy not on reference, how to make it works on reference?
+
+isBinary::(Ord a) => Tree a -> Bool
+isBinary Null = True
+isBinary (Node a Null Null) = True
+isBinary (Node a (Node b Null Null) Null)
+    |a>b = True
+    |a==b = True
+    |a<b = False
+isBinary (Node a Null (Node b Null Null))
+    |a<b = True
+    |a==b = False
+    |a>b = False
+isBinary (Node a (Node b leftB rightB) Null)
+    |a<b = False
+    |otherwise = isBinary(Node b leftB rightB)
+isBinary (Node a Null (Node c leftC rightC))
+    |a>c = False
+    |a==c = False
+    |otherwise = isBinary(Node c leftC rightC)
+isBinary (Node a (Node b leftB rightB) (Node c leftC rightC))--(Node b leftB rightB) not match Null? why>??
+    |a<b = False
+    |a>c = False
+    |a==c = False
+    |otherwise = (isBinary(Node b leftB rightB) && isBinary(Node c leftC rightC))
